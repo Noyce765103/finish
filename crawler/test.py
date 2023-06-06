600a0671c8b81105532b6f92ed8904f66e71c96e
noyce <noyce.liujiaqi@outlook.com>
2023-06-07 00:39:53 +0800

    test

--- crawler/test.py
+++ crawler/test.py
@@ -1,6 +1,5 @@
 import requests
 from main import parse_youlai, Config
-from lxml import etree
 
 TEST_URL = "https://www.youlai.cn/dise/1134.html"
 rsp = requests.get(TEST_URL, headers=Config.headers)
